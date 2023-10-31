ab45133aeaf2b7778e9de9abc69fb7d146cb5719
trentNakata <78619753+trentNakata@users.noreply.github.com>
2023-10-31 22:13:37 +0000

    push

---
+++ hellonode.js
@@ -0,0 +1,15 @@
+// server.js
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3000;
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+//Set the response HTTP header with HTTP status and Content type
+res.statusCode = 200;
+res.setHeader("Content-Type", "text/html");
+res.end("<h1>Hello, Node.js!</h1>");
+});
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
